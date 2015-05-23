8dc950aa99d83f1b62dc1e8fb576467f341f7a09
Njahs <njahs@web.de>
2015-05-23 13:20:07 +0200

    Changed names

--- viewer/src/LogViewer/Properties/AssemblyInfo.cs
+++ viewer/src/LogViewer/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogViewer")]
-[assembly: AssemblyDescription("Log Viewer for Altis Life Database")]
+[assembly: AssemblyTitle("Log Viewer")]
+[assembly: AssemblyDescription("Log Viewer for Altis Life database")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
