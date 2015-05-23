f9baab27afcd4ef48be038bfd5d4584f39a3928d
Njahs <njahs@web.de>
2015-05-23 15:28:32 +0200

    Binary 1.0.1
    Some name/text changes

--- viewer/src/LogViewer/ControlWindow.xaml.cs
+++ viewer/src/LogViewer/ControlWindow.xaml.cs
@@ -21,9 +21,6 @@ using LogViewer.Main;
 
 namespace LogViewer
 {
-    /// <summary>
-    /// Interaction logic for ControlWindow.xaml
-    /// </summary>
     public partial class ControlWindow : Window
     {
         ChartWindow cW;
@@ -90,17 +87,17 @@ namespace LogViewer
                 var datesDataSource = new EnumerableDataSource<DateTime>(dates);
                 datesDataSource.SetXMapping(x => cW.dateAxis.ConvertToDouble(x));
 
-                var cashDiffDataSource = new EnumerableDataSource<int>(cash);
-                cashDiffDataSource.SetYMapping(y => y);
-                var bankDiffDataSource = new EnumerableDataSource<int>(bankCash);
-                bankDiffDataSource.SetYMapping(y => y);
+                var cashDataSource = new EnumerableDataSource<int>(cash);
+                cashDataSource.SetYMapping(y => y);
+                var bankDataSource = new EnumerableDataSource<int>(bankCash);
+                bankDataSource.SetYMapping(y => y);
                 var cashTotalDataSource = new EnumerableDataSource<int>(cashTotal);
                 cashTotalDataSource.SetYMapping(y => y);
 
                 CompositeDataSource compositeDataSource1 = new
-                  CompositeDataSource(datesDataSource, cashDiffDataSource);
+                  CompositeDataSource(datesDataSource, cashDataSource);
                 CompositeDataSource compositeDataSource2 = new
-                  CompositeDataSource(datesDataSource, bankDiffDataSource);
+                  CompositeDataSource(datesDataSource, bankDataSource);
                 CompositeDataSource compositeDataSource3 = new
                   CompositeDataSource(datesDataSource, cashTotalDataSource);
 
@@ -110,7 +107,7 @@ namespace LogViewer
                 if (Properties.Settings.Default.show_cash) cW.plotter.AddLineGraph(compositeDataSource1,
                   new Pen(Brushes.Red, lineSize),
                   new CirclePointMarker { Size = pointSize, Fill = Brushes.Red },
-                  new PenDescription("Cash difference"));
+                  new PenDescription("Cash"));
 
                 if (Properties.Settings.Default.show_bank) cW.plotter.AddLineGraph(compositeDataSource2,
                   new Pen(Brushes.Blue, lineSize),
@@ -121,12 +118,12 @@ namespace LogViewer
                       Pen = new Pen(Brushes.Black, 2.0),
                       Fill = Brushes.GreenYellow
                   },*/
-                  new PenDescription("Bank difference"));
+                  new PenDescription("Bank"));
 
                 if (Properties.Settings.Default.show_total) cW.plotter.AddLineGraph(compositeDataSource3,
                   new Pen(Brushes.DarkGreen, lineSize),
                   new CirclePointMarker { Size = pointSize, Fill = Brushes.DarkGreen },
-                  new PenDescription("Total Cash"));
+                  new PenDescription("Total cash"));
 
                 cW.plotter.Legend.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
                 cW.plotter.Legend.VerticalAlignment = System.Windows.VerticalAlignment.Top;
